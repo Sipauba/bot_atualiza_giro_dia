17f16f468fe05088dcd242200b20e2d3b3a05937
Mateus de Souza Sipauba <mateus.sipauba@sodinenet.com.br>
2023-08-31 19:19:57 -0300

    Código funcional de forma simples

--- giro_dia.py
+++ giro_dia.py
@@ -3,10 +3,10 @@ import time
 import datetime
 import cx_Oracle
 
-host = 'x.x.x.x'
-servico = 'xxxx'
-usuario = 'xxxxxx'
-senha = 'xxxxxxxxxxx'
+host = 'xx'
+servico = 'xx'
+usuario = 'xx'
+senha = 'xx'
 
 # Encontra o arquivo que aponta para o banco de dados
 cx_Oracle.init_oracle_client(lib_dir="./instantclient_21_10")
@@ -17,8 +17,12 @@ conecta_banco = cx_Oracle.connect(usuario, senha, f'{host}/{servico}')
 # Cria um cursor no banco para que seja possível fazer consultas e alterações no banco de dados
 cursor = conecta_banco.cursor()
 
-def minha_tarefa():
-    print("Tarefa executada!")
+dia_atual = datetime.datetime.now()
+
+def atualiza_giro():
+    
+    print('---------------------------------------------------')
+    print('Calculando dias uteis...')
     sql_dias_uteis = """
     SELECT 60+count(diavendas) FROM PCDIASUTEIS 
     WHERE DIAVENDAS='S' 
@@ -28,19 +32,26 @@ def minha_tarefa():
     """
     cursor.execute(sql_dias_uteis)
     dias_uteis = cursor.fetchone()[0]
+    print('Qt. dias uteis: {}'.format(dias_uteis))
     
-    sql_giro = """UPDATE PCEST 
-    SET QTGIRODIA=round((QTVENDMES+QTVENDMES1+QTVENDMES2+QTVENDMES3)/{},2)
-    WHERE CODFILIAL IN (3,4,5,6,7,70,14,17,18,19,20,61);
-    COMMIT;
-    """.format(dias_uteis)
+    print('Atualizando Giro Dia...')
+    sql_giro = 'UPDATE PCEST SET QTGIRODIA=round((QTVENDMES+QTVENDMES1+QTVENDMES2+QTVENDMES3)/{},2) WHERE CODFILIAL IN (3,4,5,6,7,70,14,17,18,19,20,61)'.format(dias_uteis)
     cursor.execute(sql_giro)
-
-# Agendar a tarefa para ser executada de segunda a sexta-feira às 10:00 AM
-for dia_da_semana in range(0, 5):  # Segunda a sexta correspondem aos dias 0 a 4
-    schedule.every().day.at("10:00").do(minha_tarefa).when(lambda: datetime.datetime.today().weekday() == dia_da_semana)
-
-# Loop para manter o programa em execução enquanto aguarda a execução das tarefas agendadas
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    print('Fazendo COMMIT...')
+    cursor.execute('COMMIT')
+    print('Giro atualizado! - {} -'.format(dia_atual))
+    print('---------------------------------------------------')
+    
+def agendamento():
+    
+    schedule.every().monday.at("07:00").do(atualiza_giro)
+    schedule.every().tuesday.at("07:00").do(atualiza_giro)
+    schedule.every().wednesday.at("07:00").do(atualiza_giro)
+    schedule.every().thursday.at("07:00").do(atualiza_giro)
+    schedule.every().friday.at("07:00").do(atualiza_giro)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    
+agendamento()
