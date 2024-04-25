ba2ecb1e8d9dc30c87312d73a8278960859d01fd
sipauba <mateus.sipauba@yahoo.com.br>
2024-04-24 21:53:54 -0300

    Na tentativa de incluir um sistema de registros de log na aplicação. A intenção é  tornar possível saber o que houve quando a aplicação for desligada. Ainda estou decidindo se coloco um select com todos os itens que foram afetados ou não. O problema é que essa consulta gera quase 700 mil linhas e faz com que o arquivo TXT fique com aproximadamente 30 megas, o que pode ser um problema a longo prazo... se compactar com winrar o valor cai pra 10% do tamanho total! Minhas primeiras tentativas de compactar automaticamente foram frustradas, mas ainda posso tentar outros métodos. Preciso também aplicar um NVL no update pra garantir que nenhum valor nulo seja incluido na equação do calculo do giro, pois se isso acontece o valor do giro também fica nulo. O NVL vai garantir que se houver valor nulo ele será considerado como ZERO.

---
+++ giro_dia_com_log.py
@@ -0,0 +1,108 @@
+import schedule
+import time
+import datetime
+import cx_Oracle
+
+
+host = ''
+servico = ''
+usuario = ''
+senha = ''
+
+# Define o diretório onde os arquivos de log serão armazenados
+diretorio_logs = "logs/"
+
+# Função para compactar o arquivo de log utilizando o WinRAR
+
+# Encontra o arquivo que aponta para o banco de dados
+cx_Oracle.init_oracle_client(lib_dir="./instantclient_21_10")
+
+# Faz a conexão ao banco de dados
+conecta_banco = cx_Oracle.connect(usuario, senha, f'{host}/{servico}')
+
+# Cria um cursor no banco para que seja possível fazer consultas e alterações no banco de dados
+cursor = conecta_banco.cursor()
+
+print('Aplicação iniciada!')
+
+def atualiza_giro():
+    dia_atual_inicio = datetime.datetime.now()
+    """
+    print('---------------------------------------------------')
+    print('Calculando dias úteis... => {}'.format(dia_atual_inicio))
+    sql_dias_uteis = """
+    #SELECT 60+count(diavendas) FROM PCDIASUTEIS 
+    #WHERE DIAVENDAS='S' 
+    #AND CODFILIAL=6 
+    #AND DATA BETWEEN TRUNC(SYSDATE, 'MM')
+    #AND TRUNC(SYSDATE) - 1
+    """
+    cursor.execute(sql_dias_uteis)
+    dias_uteis = cursor.fetchone()[0]
+    print('Qt. dias úteis: {}'.format(dias_uteis))
+
+    print('Atualizando Giro Dia...')
+    sql_giro = 'UPDATE PCEST SET QTGIRODIA=round((QTVENDMES+QTVENDMES1+QTVENDMES2+QTVENDMES3)/{},2) WHERE CODFILIAL IN (1,3,4,5,6,7,70,14,17,18,19,20,61)'.format(
+        dias_uteis)
+    cursor.execute(sql_giro)
+    print('Fazendo COMMIT...')
+    cursor.execute('COMMIT')
+    dia_atual_fim = datetime.datetime.now()
+    print('Giro atualizado! - {} -'.format(dia_atual_fim))
+    print('---------------------------------------------------')
+    """
+    # Gera o nome do arquivo de log com a data atual
+    nome_arquivo = dia_atual_inicio.strftime("%d-%m-%Y") + ".txt"
+    nome_arquivo_completo = diretorio_logs + nome_arquivo
+
+    # Executa o select para obter os dados
+    sql_select_geral = cursor.execute("""
+    SELECT CODFILIAL, CODPROD, QTEST, DTULTSAIDA, QTVENDMES, QTVENDMES1, QTVENDMES2, QTVENDMES3, QTGIRODIA 
+    FROM PCEST 
+    WHERE CODFILIAL IN (1,3,4,5,6,7,70,14,17,18,19,20,61)
+    """)
+    
+    cursor.execute("""SELECT COUNT (*) 
+                    FROM PCEST 
+                    WHERE CODFILIAL IN (1,3,4,5,6,7,70,14,17,18,19,20,61)""")
+    contagem_geral = cursor.fetchone()[0]
+    
+    cursor.execute("""SELECT COUNT (*)
+                    FROM PCEST 
+                    WHERE QTGIRODIA = 0
+                    AND CODFILIAL IN (1,3,4,5,6,7,70,14,17,18,19,20,61)""")
+    contagem_sem_giro = cursor.fetchone()[0]
+    
+    cursor.execute("""SELECT COUNT (*)
+                    FROM PCEST
+                    WHERE QTGIRODIA > 0
+                    AND CODFILIAL IN (1,3,4,5,6,7,70,14,17,18,19,20,61)""")
+    contagem_com_giro = cursor.fetchone()[0]
+
+    # Abre o arquivo de log para escrita
+    with open(nome_arquivo_completo, "w") as arquivo:
+        arquivo.write("Data e hora da atualização do giro: {}\n".format(dia_atual_inicio))
+        arquivo.write("Linhas afetadas: {}\n".format(contagem_geral))
+        arquivo.write("Itens sem giro: {}\n".format(contagem_sem_giro))
+        arquivo.write("Itens com giro: {}\n".format(contagem_com_giro))
+        arquivo.write("Data e hora do fim da atualização: {}\n".format(dia_atual_inicio))
+        """# Escreve os cabeçalhos das colunas
+        arquivo.write("CODFILIAL, CODPROD, QTEST, DTULTSAIDA, QTVENDMES, QTVENDMES1, QTVENDMES2, QTVENDMES3, QTGIRODIA\n")
+        # Escreve os dados no arquivo de log
+        for linha in cursor:
+            arquivo.write(",".join(map(str, linha)) + "\n")"""
+    
+atualiza_giro()
+
+def agendamento():
+    schedule.every().monday.at("07:00").do(atualiza_giro)
+    schedule.every().tuesday.at("07:00").do(atualiza_giro)
+    schedule.every().wednesday.at("07:00").do(atualiza_giro)
+    schedule.every().thursday.at("07:00").do(atualiza_giro)
+    schedule.every().friday.at("07:00").do(atualiza_giro)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+agendamento()
